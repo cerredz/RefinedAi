d42fd7405267f65146146cd875f42010db46ab87
cerredz <422michaelcerreto@gmail.com>
2023-08-13 01:11:07 -0400

    Created Frontend Design for Sign In Page

--- client/src/components/loginForm/LoginForm.jsx
+++ client/src/components/loginForm/LoginForm.jsx
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { BsArrowRight } from "react-icons/bs";
 import "./LoginForm.css";
 const LoginForm = (props) => {
   const [hasCreatedAccount, setHasCreatedAccount] = useState(false);
+  const [rememberUser, setRememberUser] = useState(false);
+
+  const [errors, setErrors] = useState({
+    firstName: true,
+    lastName: false,
+    username: false,
+    email: false,
+    password: false,
+    retypePassword: false,
+  });
 
   useEffect(() => {
     if (localStorage.getItem("accounts")) {
@@ -9,40 +20,55 @@ const LoginForm = (props) => {
       setHasCreatedAccount(true);
     }
   }, []);
+
+  const handleSignInClick = () => {};
+
   return (
     <div className="login-form-container">
       <form className="login-form " action="">
         <div className="form-header">
           <h1>{hasCreatedAccount ? "Login" : "Create Account"}</h1>
+          <p className="form-subtitle">
+            {hasCreatedAccount
+              ? ""
+              : "Join the community and gain access to world class upscaling services"}
+          </p>
         </div>
 
         <div className="form-details flex">
           {hasCreatedAccount ? (
             <>
-              <label>login</label>
-              <input></input>
+              <div className="form-account">
+                <input type="text" placeholder="Username" />
+                <input type="text" placeholder="Email Address" />
+                <input type="password" placeholder="Password" />
+              </div>
             </>
           ) : (
             <>
-              <label htmlFor="">First Name</label>
-              <input type="text" />
+              <div className="form-names flex">
+                <input type="text" placeholder="First Name" />
 
-              <label htmlFor="">Last Name</label>
-              <input type="text" />
-              <label htmlFor="">Username</label>
-              <input type="text" />
-              <label htmlFor="">Email Address</label>
-              <input type="text" />
-              <label htmlFor="">Password</label>
-              <input type="text" />
-              <label htmlFor="">Retype Password</label>
-              <input type="text" />
-              <p>
-                Already Have An Account? <span>Login</span>
-              </p>
-              <div className="flex">
-                <input type="checkbox" />
-                <label htmlFor="">Remember me</label>
+                <input type="text" placeholder="Last Name" />
+              </div>
+
+              <div className="form-account flex">
+                <input type="text" placeholder="Username" />
+                <input type="text" placeholder="Email Address" />
+                <input type="password" placeholder="Password" />
+
+                <input type="password" placeholder="Retype Password" />
+                <div
+                  onClick={() => handleSignInClick}
+                  className="btn-container flex "
+                >
+                  <p className="btn">Sign up</p>
+                  <BsArrowRight />
+                </div>
+                <p>
+                  Already Have An Account?{" "}
+                  <span onClick={() => setHasCreatedAccount(true)}>Login</span>
+                </p>
               </div>
             </>
           )}
